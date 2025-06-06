43e550261ec083414da25a967a504e99682f5ad6
user2 <la3089603@gmail.com>
2025-06-06 18:13:14 +0800

    changed viteconfigts & packagejson

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const REPO_NAME = '/bear/';
+
 // https://vite.dev/config/
 export default defineConfig({
+
+  base: process.env.NODE_ENV === 'production' ? REPO_NAME : '/',
+
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    outDir: 'dist',
+  }
 })
